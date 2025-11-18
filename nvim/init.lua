b4d6504192af945d4681c7c6b51ba53102468279
a-korkin <a-korkin88@yandex.ru>
2025-11-18 11:50:47 +0300

    fix: finding errors

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,6 +45,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.clipboard = "unnamedplus"
 -- vim.opt.signcolumn = "yes:1"
+vim.wo.relativenumber = true
 
 vim.g.mapleader = " "
 
@@ -65,54 +66,54 @@ require("nvim-surround").setup()
 
 
 -- Theme
-require("catppuccin").setup({
-    flavour = "auto", -- latte, frappe, macchiato, mocha
-    background = {    -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false,            -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = true,               -- Force no italic
-    no_bold = false,                -- Force no bold
-    no_underline = false,           -- Force no underline
-    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" },    -- Change the style of comments
-        conditionals = {},          --{ "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    default_integrations = true,
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-})
+-- require("catppuccin").setup({
+--     flavour = "auto", -- latte, frappe, macchiato, mocha
+--     background = {    -- :h background
+--         light = "latte",
+--         dark = "mocha",
+--     },
+--     transparent_background = false, -- disables setting the background color.
+--     show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+--     term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+--     dim_inactive = {
+--         enabled = false,            -- dims the background color of inactive window
+--         shade = "dark",
+--         percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+--     },
+--     no_italic = true,               -- Force no italic
+--     no_bold = false,                -- Force no bold
+--     no_underline = false,           -- Force no underline
+--     styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+--         comments = { "italic" },    -- Change the style of comments
+--         conditionals = {},          --{ "italic" },
+--         loops = {},
+--         functions = {},
+--         keywords = {},
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+--     },
+--     color_overrides = {},
+--     custom_highlights = {},
+--     default_integrations = true,
+--     integrations = {
+--         cmp = true,
+--         gitsigns = true,
+--         nvimtree = true,
+--         treesitter = true,
+--         notify = false,
+--         mini = {
+--             enabled = true,
+--             indentscope_color = "",
+--         },
+--         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+--     },
+-- })
 
 -- require("github-theme").setup()
 -- require("midnight").setup()
@@ -123,6 +124,7 @@ vim.o.background = "dark"
 -- vim.cmd("colorscheme github_dark_tritanopia")
 -- vim.cmd("colorscheme midnight")
 vim.cmd("colorscheme gruvbox")
+-- vim.cmd("highlight Normal guibg=#000000")
 
 require("lualine").setup({
     options = {
@@ -210,9 +212,14 @@ lsp.on_attach(function(_, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<Space>e", vim.diagnostic.open_float, bufopts)
+    -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, virtual_lines = { current_line = true } }) end,
+        opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, virtual_lines = { current_line = true } }) end,
+        opts)
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, bufopts)
     -- vim.keymap.set("i", "jj", "<Esc>", { silent = true })
 end)
 
